b3f982402623fe2f32890ca1635b0ef8cd486586
Oleksii <mrkolisnyk13@gmail.com>
2023-12-25 14:56:25 +0200

    init

---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;dbname=postgres',
+    'username' => 'ama_user',
+    'password' => '1234',
+    'charset' => 'utf8',
+
+	//'class' => 'yii\db\Connection',
+    //'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    //'username' => 'root',
+    //'password' => '',
+    //'charset' => 'utf8',
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
